54778920ba52dd2de59a7ad48076f9345d48a4a0
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-23 16:45:19 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'iwnlp',
+  packages = ['iwnlp'],
+  version = '0.1',
+  description = 'Python implementation for IWNLP',
+  author = 'Matthias Liebeck',
+  author_email = 'liebeck@cs.uni-duesseldorf.de',
+  url = 'https://github.com/Liebeck/IWNLP-py',
+  download_url = 'https://github.com/Liebeck/IWNLP-py/archive/0.1.tar.gz',
+  keywords = ['IWNLP', 'NLP', 'German', 'lemmatization', 'Wiktionary'],
+  classifiers = [],
+)
