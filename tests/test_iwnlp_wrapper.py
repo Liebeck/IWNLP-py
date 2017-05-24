79497c7524701eacc4424f69cc11c4a67cc48fa0
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-24 12:58:30 +0200

    Start with unit test coverage

---
+++ tests/test_iwnlp_wrapper.py
@@ -0,0 +1,33 @@
+import unittest
+from iwnlp.iwnlp_wrapper import IWNLPWrapper
+
+
+class IWNLPWrapperTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.iwnlp = IWNLPWrapper(lemmatizer_path='../data/IWNLP.Lemmatizer_20170501.json')
+
+    def test_lemmatize_plain_example1(self):
+        predicted = self.iwnlp.lemmatize_plain('Hallo')
+        self.assertEqual(predicted, ['Hallo'])
+
+    def test_lemmatize_plain_example2(self):
+        predicted = self.iwnlp.lemmatize_plain('Hallo', ignore_case=False)
+        self.assertEqual(predicted, ['Hallo'])
+
+    def test_lemmatize_plain_example3(self):
+        predicted = self.iwnlp.lemmatize_plain('birne', ignore_case=False)
+        self.assertEqual(predicted, None)
+
+    def test_lemmatize_plain_example4(self):
+        predicted = self.iwnlp.lemmatize_plain('birne', ignore_case=True)
+        self.assertEqual(predicted, ['Birne'])
+
+    def test_contains_entry_example1(self):
+        self.assertEqual(self.iwnlp.contains_entry('Birne'), True)
+
+    def test_contains_entry_example2(self):
+        self.assertEqual(self.iwnlp.contains_entry('birne', ignore_case=False), False)
+
+    def test_contains_entry_example3(self):
+        self.assertEqual(self.iwnlp.contains_entry('birne', ignore_case=True), True)
