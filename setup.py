62a41bab5ba91b534b9333fe81648634745edda2
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-05-24 15:13:47 +0200

    Release 0.1.4 version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'iwnlp',
   packages = ['iwnlp'],
-  version = '0.1.2',
+  version = '0.1.4',
   description = 'Python implementation for IWNLP',
   author = 'Matthias Liebeck',
   author_email = 'liebeck@cs.uni-duesseldorf.de',
   url = 'https://github.com/Liebeck/IWNLP-py',
-  download_url = 'https://github.com/Liebeck/IWNLP-py/archive/0.1.2.tar.gz',
+  download_url = 'https://github.com/Liebeck/IWNLP-py/archive/0.1.4.tar.gz',
   keywords = ['IWNLP', 'NLP', 'German', 'lemmatization', 'Wiktionary'],
   classifiers = [],
 )
