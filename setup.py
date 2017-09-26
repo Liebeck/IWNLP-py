7f15e12646870c9a19119bc4e9ac7b0223a847e2
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-09-26 13:13:34 +0200

    Increase pip version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'iwnlp',
   packages = ['iwnlp'],
-  version = '0.1.5',
+  version = '0.1.6',
   description = 'Python implementation for IWNLP',
   author = 'Matthias Liebeck',
   author_email = 'liebeck@cs.uni-duesseldorf.de',
   url = 'https://github.com/Liebeck/IWNLP-py',
-  download_url = 'https://github.com/Liebeck/IWNLP-py/archive/0.1.5.tar.gz',
+  download_url = 'https://github.com/Liebeck/IWNLP-py/archive/0.1.6.tar.gz',
   keywords = ['IWNLP', 'NLP', 'German', 'lemmatization', 'Wiktionary'],
   classifiers = [],
 )
